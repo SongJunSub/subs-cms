043d290e35fe00247c2525debb226f75632fb69c
JunSub_Dev <song911130@gmail.com>
2025-07-01 16:26:05 +0900

    feat: Spring Boot 프로젝트 초기화 (web 의존성 포함)

---
+++ src/main/java/com/songjunsub/subscms/SubsCmsApplication.java
@@ -0,0 +1,13 @@
+package com.songjunsub.subscms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubsCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/songjunsub/subscms/SubsCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songjunsub.subscms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubsCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
